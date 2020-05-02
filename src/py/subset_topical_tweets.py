b47dd149d2c052833282ff6f9d579c88a9ae0e2d
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2020-05-02 16:23:14 +0200

    subsetting to topical tweets with two methods

--- src/py/subset_topical_tweets.py
+++ src/py/subset_topical_tweets.py
@@ -6,10 +6,6 @@ from pathlib import Path
 import sys
 import numpy as np
 
-PROCESSED_PATH = ''
-DATA_SUFFIX = '.tsv'
-SEP = '\t'
-
 DATA_PATH = '/home/sentiment/data_lake/twitter/nlp_project_samples/'
 MODEL_PATH = 'models/daily_topics/'
 ASSIGNED_PATH ='data/daily_topic_distributions/'
@@ -21,13 +17,11 @@ PROB_THRESHOLD = 0.25 # lower bound on percent probability of being assigned to 
 def get_relevant_topics(model, keywords, topn=None, weight_threshold=None):
     """
     Takes HDP model and keywords along with one of either topn or weight_threshold to determine relevence
-
     arguments:
         model - gensim HdpModel
         keywords - list of strings of words we want to look for, should be stemmed
         topn - int, number of words allowed to look through for topn selection
         weight_threshold - float, threshold to cut off for relevence, word weights must be strictly greater than
-
     returns:
         relevant_topics - list of topic indices relevant to given metric
     """
@@ -43,7 +37,7 @@ def get_relevant_topics(model, keywords, topn=None, weight_threshold=None):
             if pd.Series(keywords).isin(top).any():
                 relevant_topics.append(i)
         else:
-            eligible = np.argwhere(topic > weight)
+            eligible = np.argwhere(topic > weight_threshold)
             if pd.Series(keywords).isin(eligible).any():
                 relevant_topics.append(i)
 
@@ -58,27 +52,48 @@ def is_relevant(topic_dist, relevant_topics):
             topics.append(topic_dist[:,0][i])
     return bool(set(topics) & set(relevant_topics))
 
-date = sys.argv[1]
-keywords = sys.argv[2][1:-1].replace('-', ' ').split(',')
-topic_name = sys.argv[3]
+if __name__ == '__main__':
 
-print('\nLoading data for %s' % date)
-hdp = HdpModel.load(MODEL_PATH+date+'_topics.model')
-tweets = pd.read_csv(DATA_PATH+date+DATA_SUFFIX, sep=SEP, lineterminator='\n')
-tweets.drop_duplicates('tweet_id', keep=False, inplace=True)
-topic_dists = pd.read_csv(ASSIGNED_PATH+date+DATA_SUFFIX, sep=SEP, lineterminator='\n')
-topic_dists = topic_dists[topic_dists['probability']>=PROB_THRESHOLD].reset_index(drop=True)
-print('done')
+    date = sys.argv[1]
+    keywords = sys.argv[2][1:-1].replace('-', ' ').split(',')
+    topic_name = sys.argv[3]
 
-print('Testing relevence')
-relevant_topics = get_relevant_topics(hdp, keywords, topn=TOPN)
-relevant_ids = list(set(topic_dists[topic_dists['topic'].isin(relevant_topics)]['tweet_id'].values))
-# relevant_list = [is_relevant(dist, relevant_topics) for dist in topic_dists['topic_distribution'].values]
-# relevant_ids = topic_dists['tweet_id'][relevant_list].values
-print('Done: %s topical tweets' % len(relevant_ids))
+    Path(SAVE_PATH+topic_name).mkdir(exist_ok=True)
+    with open(SAVE_PATH+topic_name+'/'+'README.md', 'w') as text_file:
+        print('Subsets created using keywords %s' % sys.argv[2], file=text_file)
 
-print('Subsetting and Saving')
-tweets = tweets[tweets['tweet_id'].isin(relevant_ids)]
-Path(SAVE_PATH+topic_name).mkdir(exist_ok=True)
-tweets.to_csv(SAVE_PATH+topic_name+'/'+date+DATA_SUFFIX, sep=SEP, index=False)
-print('Done with %s' % date)
+    print('\nSubsetting data for %s by Topic Modeling' % date)
+    tweets = pd.read_csv(DATA_PATH+date+'.tsv', sep='\t', lineterminator='\n')
+    tweets = tweets[tweets['tweet_text_stemmed'].notnull()]
+    tweets.drop_duplicates('tweet_id', keep=False, inplace=True)
+
+    hdp = HdpModel.load(MODEL_PATH+date+'_topics.model')
+    topic_dists = pd.read_csv(ASSIGNED_PATH+date+'.tsv', sep='\t', lineterminator='\n')
+    topic_dists = topic_dists[topic_dists['probability']>=PROB_THRESHOLD].reset_index(drop=True)
+
+    relevant_topics = get_relevant_topics(hdp, keywords, topn=TOPN)
+    relevant_ids = list(set(topic_dists[topic_dists['topic'].isin(relevant_topics)]['tweet_id'].values))
+    subset = tweets[tweets['tweet_id'].isin(relevant_ids)]
+
+    subset.to_csv(SAVE_PATH+topic_name+'/'+date+'_from_topic_model.tsv', sep='\t', index=False)
+    print('Done: %s topical tweets' % subset.shape[0])
+    del tweets, subset, topic_dists, hdp
+
+
+    print('\nSubsetting data for %s by String Matching' % date)
+    tweets = pd.read_csv(DATA_PATH+date+'.tsv', sep='\t', lineterminator='\n')
+    tweets = tweets[tweets['tweet_text_stemmed'].notnull()]
+    tweets.drop_duplicates('tweet_id', keep=False, inplace=True)
+
+    subset = pd.DataFrame()
+    for keyword in keywords:
+        if keyword[0]=='#' or keyword[0]=='@':
+            tweets['flag'] = [(re.search('%s\b' % keyword, elem) is not None) for elem in tweets['tweet_text_clean'].values]
+        else:
+            tweets['flag'] = [(re.search('\b%s\b' % keyword, elem) is not None) for elem in tweets['tweet_text_clean'].values]
+        subset = pd.concat([subset, tweets[tweets['flag']==True]].drop('flag', 1))
+        tweets = tweets[tweets['flag']==False].reset_index(drop=True).drop('flag', 1)
+
+    subset.to_csv(SAVE_PATH+topic_name+'/'+date+'_from_string_match.tsv', sep='\t', index=False)
+    print('Done: %s topical tweets' % subset.shape[0])
+    del tweets, subset
