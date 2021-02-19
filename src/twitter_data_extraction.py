603b374adb60e9b3602764f406e149c6a155bb6f
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2021-02-18 20:17:45 -0500

    bulk upload

---
+++ src/twitter_data_extraction.py
@@ -0,0 +1,101 @@
+#usage: python3 subset_topical_tweets.py date [key,word,list,two-words] topic_name
+# --text_path '/home/sentiment/data_lake/twitter/processed/'
+
+import pandas as pd
+import re
+from pathlib import Path
+import numpy as np
+import argparse
+import sys
+from sentence_transformers import SentenceTransformer
+
+from src.utils.extraction_utils import get_string_match_subset, get_topic_model_subset, get_dates
+
+def save_embedding(corpus, args):
+    embeddings = model.encode(corpus, show_progress_bar=True, batch_size=args.batch_size)
+    np.save('data/topical_tweets/{}{}_embeddings.npy'.format(args.ext_name, args.suffix), np.array(embeddings))
+
+def get_data():
+
+    try:
+        text = pd.read_csv(args.text_path+"{}{}{}.tsv.gz".format(date.year), sep='\t', usecols=['tweet_id', 'lang', 'tweet_text_clean', 'tweet_text_keywords'])
+        if args.lang is not None:
+            text = text[text['lang']==args.lang.lower()]
+        text = text[text['tweet_text_keywords'].notnull()]
+
+        geo = pd.read_csv(args.geo_path + '{}-{}-{}.tsv.gz'.format(date.year, str(date.month).zfill(2), str(date.day).zfill(2)), sep=',', usecols=['tweet_id', 'seder_id', ''])
+        if args.country_subset is not None:
+            geo = geo[geo['country']==args.country.upper()]
+
+        tweets = pd.merge(text, geo, how='inner', on='tweet_id')
+        del text, geo
+
+    except:
+        print("\nNo data for {}.".format(date))
+        return pd.DataFrame()
+
+    if args.topic_model:
+        print('\nSubsetting data for %s by Topic Modeling' % args.date)
+        subset = get_topic_model_subset(tweets, args)
+        print('Done: %s topical tweets' % subset.shape[0])
+    else:
+        print('Subsetting data for %s by String Matching' % args.date)
+        subset = get_string_match_subset(tweets, args)
+        print('Done: %s topical tweets' % subset.shape[0])
+
+    return subset
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('ext_name', help='what is the extraction name?')
+    parser.add_argument('--suffix', default = '', type=str, help='suffix to file names')
+
+    ## Subsetting
+    parser.add_argument('--incl_keywords', nargs='*', default='', help='Which keywords do you want to include in the extraction?')
+    parser.add_argument('--excl_keywords', nargs='*', default='', help='Which keywords do you want to exclude in the extraction?')
+    parser.add_argument('--country', default = None, type = str, help='subset to a specific country?')
+    parser.add_argument('--lang', default = None, type = str, help='subset to a specific language?')
+
+    ## Date range
+    parser.add_argument('--start_date', default='2019-01-01', help='what start date?')
+    parser.add_argument('--end_date', default='2019-12-31', help='what end date?')
+
+    ## Paths
+    parser.add_argument('--text_path', default='', help='path to twitter text data')
+    parser.add_argument('--geo_path', default='', help='path to twitter geography data')
+
+    ## Topic Model
+    parser.add_argument('--topic_model', default = False, type = bool, help='do we also run the version from topic modeling (or just string matching)?')
+    parser.add_argument('--topn', default = 30, type = int, help='number of words to look through when deciding if a topic is relevant')
+    parser.add_argument('--prob_threshold', default = 0.25, type = float, help='lower bound on percent probability of being assigned to a given topic')
+
+    ## Embeddings
+    parser.add_argument('--model', default = 'distilbert-base-nli-stsb-mean-tokens', type = str, help='embedding model')
+    parser.add_argument('--batch_size', default = 100, type = int, help='batch size')
+    parser.add_argument('--topic_model', default = False, type = bool, help='do we also run the version from topic modeling?')
+    parser.add_argument('--remove_keyword', default = False, type = bool, help='do we also run the version without the keyword?')
+    args = parser.parse_args()
+
+    args.incl_keywords = list(args.incl_keywords)
+    args.excl_keywords = list(args.excl_keywords)
+
+    if args.topic_model:
+        args.suffix += "_from_topic_model"
+
+    with open('data/topical_tweets/{}{}_README.md'.format(args.ext_name, args.suffix), 'w') as text_file:
+        print('Subsets created using arguments:\n{}'.format(args), file=text_file)
+
+    dates = get_dates(args)
+
+    df = pd.DataFrame()
+    for date in tqdm(dates):
+        temp = get_data(date, args)
+        df = pd.concat([df, subset], axis=0)
+
+    df.to_csv('data/topical_tweets/{}{}.tsv'.format(args.ext_name, args.suffix), sep='\t', index=False)
+
+    model = SentenceTransformer(args.model)
+    save_embedding(df['tweet_text_clean'].fillna('').values, args=args)
---
+++ src/utils/extraction_utils.py
@@ -0,0 +1,69 @@
+from gensim.models import HdpModel
+import pandas as pd
+import re
+from pathlib import Path
+import numpy as np
+from datetime import date, timedelta
+
+def get_dates(args):
+
+    start_date = date(int(args.start_date[:4]), int(args.start_date[5:7]), int(args.start_date[8:]))
+    end_date = date(int(args.end_date[:4]), int(args.end_date[5:7]), int(args.end_date[8:]))
+
+    dates = [start_date + timedelta(days=i) for i in range((end_date - start_date).days +1)]
+
+    return dates
+
+def get_relevant_topics(model, keywords, topn=None, weight_threshold=None):
+    """
+    Takes HDP model and keywords along with one of either topn or weight_threshold to determine relevence
+    arguments:
+        model - gensim HdpModel
+        keywords - list of strings of words we want to look for, should be stemmed
+        topn - int, number of words allowed to look through for topn selection
+        weight_threshold - float, threshold to cut off for relevence, word weights must be strictly greater than
+    returns:
+        relevant_topics - list of topic indices relevant to given metric
+    """
+    if topn is None and weight_threshold is None:
+        raise ValueError('One of topn or weight_threshold required')
+    topic_term = model.get_topics() #topic term matrix of weights num_topics x num terms
+    keywords = np.array(model.id2word.doc2idx(keywords)) #makes keywords into id format
+    relevant_topics = []
+    i= 0
+    for topic in topic_term:
+        if topn is not None:
+            top = np.argsort(topic)[-topn:]
+            if pd.Series(keywords).isin(top).any():
+                relevant_topics.append(i)
+        else:
+            eligible = np.argwhere(topic > weight_threshold)
+            if pd.Series(keywords).isin(eligible).any():
+                relevant_topics.append(i)
+
+        i+=1
+    return relevant_topics
+
+def get_topic_model_subset(tweets, args):
+    hdp = HdpModel.load('models/daily_topics/'+args.date+'_topics.model')
+    topic_dists = pd.read_csv('data/daily_topic_distributions/'+args.date+'.tsv', sep='\t', lineterminator='\n')
+    topic_dists = topic_dists[topic_dists['probability']>=args.prob_threshold].reset_index(drop=True)
+
+    relevant_topics = get_relevant_topics(hdp, args.keywords, topn=args.topn)
+    relevant_ids = list(set(topic_dists[topic_dists['topic'].isin(relevant_topics)]['tweet_id'].values))
+    subset = tweets[tweets['tweet_id'].isin(relevant_ids)]
+
+    return subset
+
+def get_string_match_subset(tweets, args):
+
+    subset = pd.DataFrame()
+    for keyword in args.keywords:
+        if keyword[0]=='#' or keyword[0]=='@':
+            tweets['flag'] = [(re.search(r'{}\b'.format(keyword), elem) is not None) for elem in tweets['tweet_text_stemmed'].values]
+        else:
+            tweets['flag'] = [(re.search(r'\b{}\b'.format(keyword), elem) is not None) for elem in tweets['tweet_text_stemmed'].values]
+        subset = pd.concat([subset, tweets[tweets['flag']==True].drop('flag', 1)], axis=0)
+        tweets = tweets[tweets['flag']==False].reset_index(drop=True).drop('flag', 1)
+
+    return subset
